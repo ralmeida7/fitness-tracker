5f62b8b2c8af19ba5f6a39ae0e9ee720dd9be13a
Roberto Almeida <roberto@Robertos-iMac.local>
2018-05-30 20:10:29 -0600

    commit inicial

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material'
+
+@NgModule({
+    imports:[MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+    exports:[MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+})
+
+export class MaterialModule {}
